8402c6ddd0ecdb277049c95e1a952ba8ad020cbf
Dr. Cat <dr.cat911@generative.xyz>
2023-05-15 13:17:45 +0700

    add multi art on project

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 
-import htmlMinifier from 'vite-plugin-html-minifier'
+import htmlMinifier from 'vite-plugin-html-minifier';
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
     plugins: [
+        createHtmlPlugin({
+            pages: [
+                {
+                    filename: 'index.html',
+                    template: 'index.html',
+                    injectOptions: {
+                        data: {
+                            title: 'Index',
+                        },
+                    },
+                },
+                {
+                    filename: 'geological.html',
+                    template: 'geological.html',
+                    injectOptions: {
+                        data: {
+                            title: 'Geological Art',
+                        }
+                    },
+                },
+            ],
+        }),
         htmlMinifier({
             minify: true,
         }),
